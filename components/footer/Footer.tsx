971529b82ed4a78f2f1176a07ca52949e0220e01
daylight2022 <1514853542@qq.com>
2024-12-23 22:54:01 +0800

    升级三方库版本；完善 footer组件的布局和样式表现

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,20 +1,29 @@
+'use client';
 import FooterLinks from './FooterLinks';
 import FooterProducts from './FooterProducts';
 import { siteConfig } from '@/config/site';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Footer = () => {
-	const d = new Date();
-	const currentYear = d.getFullYear();
+	const [currentYear, setCurrentYear] = useState<string>('');
+
+	useEffect(() => {
+		setCurrentYear(new Date().getFullYear().toString());
+	}, []);
+
 	const { authors } = siteConfig;
 	return (
-		<footer className="mt-16 space-y-2 pt-6 pb-4 flex flex-col items-center bg-black text-sm text-gray-400 border-t">
+		<footer className="mt-auto space-y-2 pt-6 pb-4 flex flex-col items-center border-t dark:border-gray-800 border-gray-200 dark:bg-background bg-gray-50 text-sm dark:text-gray-400 text-gray-600">
 			<div>
 				<FooterLinks></FooterLinks>
 				<FooterProducts></FooterProducts>
 				<div className="flex space-x-2">
-					<div>{`@${currentYear}`}&nbsp;</div>
-					<Link href={authors[0].twitter || authors[0].url} target="_blank">
+					{currentYear && `© ${currentYear}`}
+					<Link
+						href={authors[0].twitter || authors[0].url}
+						target="_blank"
+						className="hover:text-gray-900 dark:hover:text-gray-300 transition-colors">
 						{authors[0].name}&nbsp;
 					</Link>
 					<div>All rights reserved.</div>
